d0678de4b828488d8178b91dc7903b34396b2254
Perekatee <ivan.vedyaykin@yandex.ru>
2024-01-12 21:17:49 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket;
+        int mille;
+        int bonus;
+
+        ticket = 56740;
+        mille = 20;
+        bonus = ticket/mille;
+
+        System.out.println(bonus);
+
+    }
+}
